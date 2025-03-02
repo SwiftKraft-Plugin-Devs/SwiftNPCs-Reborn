d48a65b5814bcbc10fce70b6053ad9436a0d5512
SwiftKraft <76916776+SwiftKraft@users.noreply.github.com>
2025-03-02 14:51:56 -0500

    a

--- Features/NPCPathfinder.cs
+++ Features/NPCPathfinder.cs
@@ -1,5 +1,4 @@
-﻿using LabApi.Features.Console;
-using LabApi.Features.Wrappers;
+﻿using LabApi.Features.Wrappers;
 using UnityEngine;
 using UnityEngine.AI;
 using Logger = LabApi.Features.Console.Logger;
@@ -17,18 +16,19 @@ namespace SwiftNPCs.Features
             get => _destination;
             set
             {
-                _destination = !NavMesh.SamplePosition(value, out NavMeshHit hit, 3f, NavMesh.AllAreas) ? value : hit.position;
-                UpdatePath();
+                _destination = !NavMesh.SamplePosition(value, out NavMeshHit hit, 2f, NavMesh.AllAreas) ? value : hit.position;
+                Path.UpdatePath();
             }
         }
         Vector3 _destination;
 
         public float RepathTimer = 1f;
         float repathTimer;
-        public readonly NPCPath Path = new();
+        public NPCPath Path { get; private set; }
 
         public override void Begin()
         {
+            Path = new(this);
             Motor = Core.GetNPCComponent<NPCMotor>();
             Destination = Core.Position;
         }
@@ -42,13 +42,13 @@ namespace SwiftNPCs.Features
             repathTimer -= Time.fixedDeltaTime;
             if (repathTimer <= 0f)
             {
-                UpdatePath();
+                Path.UpdatePath();
                 repathTimer = RepathTimer;
             }
             else
-                Path.UpdateWaypoint(Core.Position);
+                Path.UpdateWaypoint();
 
-            Vector3 waypoint = Path.GetCurrentWaypoint(Core.Position);
+            Vector3 waypoint = Path.GetCurrentWaypoint();
             Vector3 pos = Core.Position;
             waypoint.y = 0f;
             pos.y = 0f;
@@ -59,10 +59,10 @@ namespace SwiftNPCs.Features
                 Motor.WishLookDirection = direction;
         }
 
-        public void UpdatePath() => Path.UpdatePath(Core.Position, Destination);
-
-        public class NPCPath(float waypointRadius = 1f)
+        public class NPCPath(NPCPathfinder parent, float waypointRadius = 1f)
         {
+            public readonly NPCPathfinder Parent = parent;
+
             public readonly NavMeshPath CurrentPath = new();
 
             public float WaypointRadius = waypointRadius;
@@ -76,16 +76,16 @@ namespace SwiftNPCs.Features
             }
             private int current;
 
-            public void UpdatePath(Vector3 current, Vector3 target)
+            public void UpdatePath()
             {
-                NavMesh.SamplePosition(current, out NavMeshHit hit, 2f, NavMesh.AllAreas);
-                NavMesh.CalculatePath(hit.position, target, NavMesh.AllAreas, CurrentPath);
+                NavMesh.SamplePosition(Parent.Core.Position, out NavMeshHit hit, 2f, NavMesh.AllAreas);
+                NavMesh.CalculatePath(hit.position, Parent.Destination, NavMesh.AllAreas, CurrentPath);
                 Current = 0;
                 Ended = false;
-                UpdateWaypoint(current);
+                UpdateWaypoint();
             }
 
-            public void UpdateWaypoint(Vector3 pos)
+            public void UpdateWaypoint()
             {
                 if (Ended)
                     return;
@@ -96,11 +96,17 @@ namespace SwiftNPCs.Features
                     return;
                 }
 
-                if ((GetCurrentWaypoint(pos) - pos).sqrMagnitude < WaypointRadius * WaypointRadius)
+                if ((GetCurrentWaypoint() - Parent.Core.Position).sqrMagnitude < WaypointRadius * WaypointRadius)
                     Current++;
             }
 
-            public Vector3 GetCurrentWaypoint(Vector3 d = default) => CurrentPath.corners.Length <= 0 ? d : CurrentPath.corners[Current];
+            public Vector3 GetCurrentWaypoint() => 
+                (CurrentPath.corners.Length - 1 <= Current 
+                && (Parent.Destination - Parent.Core.Position).sqrMagnitude 
+                    > WaypointRadius * WaypointRadius) 
+                || CurrentPath.corners.Length <= 0
+                    ? Parent.Destination
+                    : CurrentPath.corners[Current];
         }
     }
 }
